4a4d57a2056613598dd1521d455af590f951cf6f
narayan99883 <pandeyavi1@gmail.com>
2022-11-20 15:41:59 -0500

    comments on movement functions

--- include/setup/util/MovementFunctions.h
+++ include/setup/util/MovementFunctions.h
@@ -1,3 +1,4 @@
+//Pure pursuit function (in progress)
 void movePath();
 
 void goForward(double amount);
@@ -9,17 +10,17 @@ void turnLeft(double amount);
 void forwardVelocity(double velocity);
 void forwardForDistance(double amount, double speed);
 
+//Intermediate functions (movement)
 void turnPID(double targetTheta);
 void preciseTurnPID(double targetTheta);
 void goForwardPID(double distance);
 void fastGoForwardPID(double distance);
 
+//Summative functions
 void moveToPoint(double x, double y);
 void backToPoint(double x, double y);
 void preciseBackToPoint(double x, double y);
 
-void DriverBalancePID();
-void driverMovement();
-
+//Calculation functions
 double getAngle(double pointX, double pointY);
 double getLength(double pointX, double pointY);
